e849030b229d1682b0791ac483930f47005d2df6
Cyfire <39649916+creater0822@users.noreply.github.com>
2023-11-04 22:17:24 +0100

    Disable dom.battery Disable face detection Remove disabling dom.gamepad

--- config.js
+++ config.js
@@ -366,12 +366,13 @@ lockPref("extensions.quarantinedDomains.enabled", false); // [DEFAULT: true]
 // DON'T BOTHER: FINGERPRINTING
 // >>>>>>>>>>>>>>>>>>>>>
 //
+lockPref("camera.control.face_detection.enabled", false);
 lockPref("device.sensors.enabled", false);
 lockPref("device.sensors.ambientLight.enabled", false);
 lockPref("device.sensors.motion.enabled", false);
 lockPref("device.sensors.orientation.enabled", false);
 lockPref("device.sensors.proximity.enabled", false);
-lockPref("dom.gamepad.enabled", false);
+lockPref("dom.battery.enabled", false);
 lockPref("dom.vibrator.enabled", false);
 //
 // -------------------------------------
@@ -401,9 +402,8 @@ lockPref("browser.newtabpage.introShown", true);
 lockPref("browser.newtabpage.activity-stream.feeds.section.topstories", false); // Recommended by Pocket
 lockPref("browser.newtabpage.activity-stream.section.highlights.includePocket", false);
 lockPref("browser.newtabpage.activity-stream.showSearch", false);
-//
+// -------------------------------------
 // Disable pocket
-//
 lockPref("extensions.pocket.enabled", false); // Pocket Account [FF46+]
 lockPref("extensions.pocket.api"," ");
 lockPref("extensions.pocket.bffApi", "");
@@ -416,9 +416,8 @@ lockPref("extensions.pocket.site", " ");
 lockPref("browser.newtabpage.activity-stream.discoverystream.saveToPocketCard.enabled", false);
 lockPref("browser.newtabpage.activity-stream.discoverystream.saveToPocketCardRegions", "");
 lockPref("browser.newtabpage.activity-stream.discoverystream.sendToPocket.enabled", false);
-//
+// -------------------------------------
 // Disable Firefox accounts
-//
 lockPref("identity.fxaccounts.enabled", false); // Firefox Accounts & Sync [FF60+] [RESTART]
 lockPref("identity.fxaccounts.auth.uri", " ");
 lockPref("identity.fxaccounts.pairing.enabled", false);
@@ -428,29 +427,25 @@ lockPref("identity.fxaccounts.remote.profile.uri", " ");
 lockPref("identity.fxaccounts.remote.root", " ");
 lockPref("identity.fxaccounts.toolbar.enabled", false);
 lockPref("browser.newtabpage.activity-stream.fxaccounts.endpoint", "");
-//
+// -------------------------------------
 // More telemetry
-//
+lockPref("browser.search.serpEventTelemetry.enabled", false);
 lockPref("dom.security.unexpected_system_load_telemetry_enabled", false);
 lockPref("network.trr.confirmation_telemetry_enabled", false);
 lockPref("security.app_menu.recordEventTelemetry", false);
 lockPref("security.certerrors.recordEventTelemetry", false);
 lockPref("security.protectionspopup.recordEventTelemetry", false);
 lockPref("loop.logDomains", false);									// Disable Firefox Hello metrics collection
-//
+// -------------------------------------
 // Worthless features
-//
-lockPref("browser.search.serpEventTelemetry.enabled", false);
 lockPref("browser.tabs.firefox-view", false);       // Disable Firefox View
 lockPref("dom.flyweb.enabled", false);              // Disable flyweb
-//
+// -------------------------------------
 // Greasemonkey telemetry
-//
 lockPref("extensions.greasemonkey.stats.optedin", false);
 lockPref("extensions.greasemonkey.stats.url", "");
-//
+// -------------------------------------
 // OTHER
-//
 lockPref("browser.aboutwelcome.enabled", false);                  // disable Intro screens
 lockPref("browser.chrome.toolbar_tips", false);                   // Disable useless UI-tooltips
 lockPref("browser.compactmode.show", true);                       // Enable compact mode to be used
@@ -467,6 +462,8 @@ lockPref("privacy.usercontext.about_newtab_segregation.enabled", true);
 lockPref("security.family_safety.mode", 0);                       // Disable Windows Microsoft Family Safety cert [FF50+] [WINDOWS]
 lockPref("security.tls.version.enable-deprecated", false); 				// 6010: enforce no TLS 1.0/1.1 downgrades
 lockPref("toolkit.coverage.enabled", false);
+//
+// END OF LOCKED SETTINGS
 //
 pref("_config.js", "End of lockPref section.");
 /******************************** System-default settings ********************************/
@@ -491,15 +488,15 @@ pref("_config.js", "End of reset-on-restart section.");
 /******************************** User-overwritable settings ********************************/
 // Default user interaction prefs
 defaultPref("browser.uiCustomization.state", "{\"placements\":{\"widget-overflow-fixed-list\":[],\"unified-extensions-area\":[\"_a6c4a591-f1b2-4f03-b3ff-767e5bedf4e7_-browser-action\",\"_f209234a-76f0-4735-9920-eb62507a54cd_-browser-action\",\"enhancerforyoutube_maximerf_addons_mozilla_org-browser-action\",\"treestyletab_piro_sakura_ne_jp-browser-action\",\"keepassxc-browser_keepassxc_org-browser-action\",\"_0981817c-71b3-4853-a801-481c90af2e8e_-browser-action\"],\"nav-bar\":[\"back-button\",\"forward-button\",\"stop-reload-button\",\"addon_darkreader_org-browser-action\",\"customizableui-special-spring1\",\"7esoorv3_alefvanoon_anonaddy_me-browser-action\",\"pagehacker-nico_nc-browser-action\",\"_6ac85730-7d0f-4de0-b3fa-21142dd85326_-browser-action\",\"developer-button\",\"urlbar-container\",\"downloads-button\",\"_ddc359d1-844a-42a7-9aa1-88a850a938a8_-browser-action\",\"_63d150c4-394c-4275-bc32-c464e76a891c_-browser-action\",\"_66d854c2-fd1b-4857-bd0a-7d220e4834da_-browser-action\",\"simple-translate_sienori-browser-action\",\"customizableui-special-spring2\",\"save-to-pocket-button\",\"ublock0_raymondhill_net-browser-action\",\"fxa-toolbar-menu-button\",\"jid1-y3wfe7td45awdw_jetpack-browser-action\",\"_9350bc42-47fb-4598-ae0f-825e3dd9ceba_-browser-action\",\"skipredirect_sblask-browser-action\",\"_aecec67f-0d10-4fa7-b7c7-609a2db280cf_-browser-action\",\"smart-referer_meh_paranoid_pk-browser-action\",\"canvasblocker_kkapsner_de-browser-action\",\"unified-extensions-button\"],\"toolbar-menubar\":[\"menubar-items\"],\"TabsToolbar\":[\"firefox-view-button\",\"sidebar-button\",\"save-page-button\",\"tabbrowser-tabs\",\"new-tab-button\",\"alltabs-button\",\"_c607c8df-14a7-4f28-894f-29e8722976af_-browser-action\",\"_testpilot-containers-browser-action\",\"profile-switcher-ff_nd_ax-browser-action\"],\"PersonalToolbar\":[\"import-button\",\"personal-bookmarks\",\"_2e5ff8c8-32fe-46d0-9fc8-6b8986621f3c_-browser-action\",\"zoom-controls\",\"_d07ccf11-c0cd-4938-a265-2a4d6ad01189_-browser-action\"]},\"seen\":[\"developer-button\",\"profiler-button\",\"jid1-y3wfe7td45awdw_jetpack-browser-action\",\"_63d150c4-394c-4275-bc32-c464e76a891c_-browser-action\",\"_9350bc42-47fb-4598-ae0f-825e3dd9ceba_-browser-action\",\"_f209234a-76f0-4735-9920-eb62507a54cd_-browser-action\",\"7esoorv3_alefvanoon_anonaddy_me-browser-action\",\"_testpilot-containers-browser-action\",\"enhancerforyoutube_maximerf_addons_mozilla_org-browser-action\",\"addon_darkreader_org-browser-action\",\"canvasblocker_kkapsner_de-browser-action\",\"treestyletab_piro_sakura_ne_jp-browser-action\",\"pagehacker-nico_nc-browser-action\",\"keepassxc-browser_keepassxc_org-browser-action\",\"smart-referer_meh_paranoid_pk-browser-action\",\"_2e5ff8c8-32fe-46d0-9fc8-6b8986621f3c_-browser-action\",\"_66d854c2-fd1b-4857-bd0a-7d220e4834da_-browser-action\",\"_6ac85730-7d0f-4de0-b3fa-21142dd85326_-browser-action\",\"_a6c4a591-f1b2-4f03-b3ff-767e5bedf4e7_-browser-action\",\"_aecec67f-0d10-4fa7-b7c7-609a2db280cf_-browser-action\",\"_ddc359d1-844a-42a7-9aa1-88a850a938a8_-browser-action\",\"_c607c8df-14a7-4f28-894f-29e8722976af_-browser-action\",\"_d07ccf11-c0cd-4938-a265-2a4d6ad01189_-browser-action\",\"ublock0_raymondhill_net-browser-action\",\"_0981817c-71b3-4853-a801-481c90af2e8e_-browser-action\",\"simple-translate_sienori-browser-action\",\"profile-switcher-ff_nd_ax-browser-action\",\"skipredirect_sblask-browser-action\"],\"dirtyAreaCache\":[\"unified-extensions-area\",\"nav-bar\",\"TabsToolbar\",\"PersonalToolbar\",\"toolbar-menubar\"],\"currentVersion\":19,\"newElementCount\":8}");
-defaultPref("browser.uidensity", 1); 							// Defaults to compact UI
-defaultPref("browser.search.openintab", true);					// Open search in new tab
-defaultPref("browser.tabs.closeWindowWithLastTab", false);		// Don't close the window with the last tab
-defaultPref("layout.spellcheckDefault", 2);						// Use spellcheck in every textbox
+defaultPref("browser.uidensity", 1); 							              // Defaults to compact UI
+defaultPref("browser.search.openintab", true);					        // Open search in new tab
+defaultPref("browser.tabs.closeWindowWithLastTab", false);		  // Don't close the window with the last tab
+defaultPref("layout.spellcheckDefault", 2);						          // Use spellcheck in every textbox
 
 // PREF: Enable useful URL-bar features
-defaultPref("keyword.enabled", true);                   		// 0801 enabled urlbar search
-defaultPref("browser.search.suggest.enabled", true);    		// 0804 live search suggestions
-defaultPref("browser.urlbar.suggest.topsites", false);			// 5010 Disable dropdown suggestions with empty query
+defaultPref("keyword.enabled", true);                   		    // 0801 enabled urlbar search
+defaultPref("browser.search.suggest.enabled", true);    		    // 0804 live search suggestions
+defaultPref("browser.urlbar.suggest.topsites", false);			    // 5010 Disable dropdown suggestions with empty query
 defaultPref("browser.urlbar.suggest.calculator", true);
 defaultPref("browser.urlbar.unitConversion.enabled", true);
 defaultPref("browser.urlbar.quicksuggest.enabled", false);
@@ -507,12 +504,12 @@ defaultPref("browser.urlbar.groupLabels.enabled", true);
 defaultPref("browser.urlbar.trimURLs", false);
 
 // PREF: Download settings
-defaultPref("browser.download.useDownloadDir", false);			// Always ask where to save the download
+defaultPref("browser.download.useDownloadDir", false);			    // Always ask where to save the download
 //defaultPref("browser.safebrowsing.downloads.enabled", false);
 
 // PREF: Bookmark settings
-defaultPref("browser.tabs.loadBookmarksInTabs", true);			// force bookmarks to open in a new tab, not the current tab
-defaultPref("browser.bookmarks.openInTabClosesMenu", false);	// leave Bookmarks Menu open when selecting a site
+defaultPref("browser.tabs.loadBookmarksInTabs", true);			    // force bookmarks to open in a new tab, not the current tab
+defaultPref("browser.bookmarks.openInTabClosesMenu", false);	  // leave Bookmarks Menu open when selecting a site
 defaultPref("browser.toolbars.bookmarks.visibility", "always");
 
 // PREF: Translation settings
@@ -545,8 +542,8 @@ defaultPref("experiments.supported", false);
 defaultPref("network.allow-experiments", false);
 
 // PREF: Smooth scrolling recommended for 60hz+ displays
-defaultPref("general.smoothScroll", true); 					// DEFAULT
-defaultPref("mousewheel.default.delta_multiplier_y", 275);  // 250-400
+defaultPref("general.smoothScroll", true); 					            // DEFAULT
+defaultPref("mousewheel.default.delta_multiplier_y", 275);      // 250-400
 
 pref("_config.js", "End of overwritable section.");
 /******************************** Firefox userChromeJS: ********************************/
